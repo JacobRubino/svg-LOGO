6a40001c73940dc6113347a5dc79bb02e86fa36f
Jacob Rubino <jacobrubino@gmail.com>
2023-06-21 22:20:37 -0400

    got inquirer to work again

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
-const { default: inquirer } = require('inquirer');
+const inquirer = require('inquirer');
 const { Triangle, Circle, Square } = require('./assets/shapes');
-const { validateTextChoice } = require('./assets/validateTextChoice');
-// const svg = require('svg') this is only for website use right? 
-const userPrompts =[
+// const { validateTextChoice } = require('./assets/validateTextChoice');
+
+const userPrompts = [
   {
     type: 'input',
     name: 'text',
-    message: 'What letters would you like dispalyed on your logo, up to 3?'
+    message: 'What letters would you like displayed on your logo, up to 3?',
   },
   {
     type: 'input',
@@ -17,38 +17,40 @@ const userPrompts =[
   {
     type: 'list',
     name: 'shape',
-    message: 'what shape would you like on your logo?', 
+    message: 'What shape would you like on your logo?', 
     choices: ['Circle', 'Triangle', 'Square'],
   },
   {
     type: 'input',
     name: 'text-color',
-    message: 'What color would you like your logos text to be?'
+    message: 'What color would you like your logo\'s text to be?',
   },
-]
+];
 
-class renderLogo {
+class RenderLogo {
   constructor() {
     this.text = '';
-    this.shapes = '';
-  };
+  }
+
   render() {
-    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"> ${this.userShape}  ${this.userText}</svg>`
-  };
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.userShape} ${this.userText}</svg>`;
+  }
+
   newText(text, color) {
-    this.userText = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}"> ${text}</text>`
-  };
+    this.userText = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
+  }
+
   newShape(shape) {
     this.userShape = shape.render();
-  };
-};
+  }
+}
 
 function init() {
   inquirer.prompt(userPrompts)
     .then((answers) => {
-      const logo = new renderLogo();
+      const logo = new RenderLogo();
       logo.newText(answers.text, answers['text-color']);
-      logo.newShape(createShapeInstance(answers['what shape would you like on your logo?']));
+      logo.newShape(createShapeInstance(answers.shape));
       return logo.render();
     })
     .then((svgContent) => {
@@ -62,6 +64,7 @@ function init() {
       console.log(error);
     });
 }
+
 function writeSVG(file, data) {
   return fs.writeFile(file, data);
 }
